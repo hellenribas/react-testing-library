625e6944c8607b775123760ecb020a736c8328e2
hellenribas <tchwtrabalho@gmail.com>
2022-03-22 17:14:06 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it(`Teste se é exibido na tela a 
+  mensagem No favorite pokemon found, se a pessoa não tiver pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = screen.getByText(/No favorite pokemon found/i);
+
+    expect(text).toBeDefined();
+  });
+});
